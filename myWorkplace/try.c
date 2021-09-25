010776bfdb9e7d9f1797ec28d62f98742c123f39
emil <85098369+em427vc@users.noreply.github.com>
2021-09-25 07:39:21 +0200

    ala

--- myWorkplace/try.c
+++ myWorkplace/try.c
@@ -4,19 +4,57 @@
 #include <math.h>
 #include <string.h>
 #include <stdbool.h>
-
-#define MAX "lol"
+#include "limits.h"
+#include "stdint.h"
 #define MAX2  2000
 #define MAX(bit,place) (bit<<place)  
 
+void func(int *a){
+  *a=10;
+}
 
+struct person{
+int id;
+};
 
+void fill(struct person **one){
+(*one)->id=99;
+}
+
+void classic(struct person *peter){
+    peter->id=20;
+}
 int main(){
    
+struct person *emil;
+struct person *peter;
 
-int a;
-printf("%d %d \n",a,MAX2);
-printf("%d",MAX(1,7));
+emil=(struct person*)malloc(sizeof(emil));
+peter=(struct person*)malloc(sizeof(emil));
+
+//fill(&emil);
+
+
+//printf("%d",emil->id);
+//classic(&peter);
+//printf("%d",peter->id);
+
+//nt8_t a=12;
+
+for(int i=32000;INT16_MAX;i++){
+    
+
+   
+}
+int aa;
+aa=256;
+
+
+ printf("%d \n",289);
+//printf("%d",MAX(1,7));
+//func(&a);
+
+
 
  
 
